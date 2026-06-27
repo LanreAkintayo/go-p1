bf060c8619d49d6a9fbcf88f472eed7ee9037a47
LanreAkintayo <akintayolanre2019@gmail.com>
2026-06-27 12:29:56 +0100

    feat: implement boilerplate

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
+
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+	
+	router.Mount("/v1", v1Router)
+
+	server := &http.Server{
+		Addr:    ":" + portString,
+		Handler: router,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Server failed to start:", err)
+	}
+
+}
