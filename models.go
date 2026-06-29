0ec2d4b24c3be263487765abbfb52fa4b0d0ad9f
LanreAkintayo <akintayolanre2019@gmail.com>
2026-06-29 08:59:38 +0100

    feat: add some endpoints

--- models.go
+++ models.go
@@ -11,8 +11,17 @@ type User struct {
 	ID        uuid.UUID `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-	Name      string `json:"name"`
-	APIKey    string `json:"api_key"`
+	Name      string    `json:"name"`
+	APIKey    string    `json:"api_key"`
+}
+
+type Feed struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+	Url       string    `json:"url"`
+	UserID    uuid.UUID `json:"user_id"`
 }
 
 func databaseUserToUser(dbUser database.User) User {
@@ -23,4 +32,25 @@ func databaseUserToUser(dbUser database.User) User {
 		Name:      dbUser.Name,
 		APIKey:    dbUser.ApiKey,
 	}
+}
+
+func databaseFeedToFeed(dbFeed database.Feed) Feed {
+	return Feed{
+		ID:        dbFeed.ID,
+		CreatedAt: dbFeed.CreatedAt,
+		UpdatedAt: dbFeed.UpdatedAt,
+		Name:      dbFeed.Name,
+		Url:       dbFeed.Url,
+		UserID:    dbFeed.UserID,
+	}
+}
+
+func databaseFeedsToFeeds(dbFeeds []database.Feed) []Feed {
+
+	feeds := make([]Feed, len(dbFeeds))
+	for i, dbFeed := range dbFeeds {
+		feeds[i] = databaseFeedToFeed(dbFeed)
+	}
+	return feeds
+
 }
