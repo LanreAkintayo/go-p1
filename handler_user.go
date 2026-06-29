9e3be0f6c21423bafc39d3cdb65b812f15a4c364
LanreAkintayo <akintayolanre2019@gmail.com>
2026-06-29 18:04:20 +0100

    feat: implement post endpoints

--- handler_user.go
+++ handler_user.go
@@ -3,10 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"net/http"
-	"time"
 	"github.com/LanreAkintayo/go-p1/internal/database"
 	"github.com/google/uuid"
+	"net/http"
+	"time"
 )
 
 func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
@@ -41,4 +41,18 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 
 func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
 	respondWithJSON(w, 200, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetPostsForUser(w http.ResponseWriter, r *http.Request, user database.User) {
+	posts, err := apiCfg.DB.GetPostsForUser(r.Context(), database.GetPostsForUserParams{
+		UserID: user.ID,
+		Limit:  10,
+	})
+
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error getting posts for user: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 200, databasePostsToPosts(posts))
 }
--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -18,3 +18,11 @@ INSERT INTO posts (
     $7,
     $8
 ) RETURNING *; 
+
+-- name: GetPostsForUser :many
+SELECT posts.* FROM posts 
+JOIN feed_follows ON posts.feed_id = feed_follows.feed_id
+WHERE feed_follows.user_id = $1
+ORDER BY posts.published_at DESC
+LIMIT $2;
+
